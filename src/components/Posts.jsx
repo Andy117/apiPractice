9b868bc9e5c08987b27f1525b910343784670789
Anderson Velasquez <andersonjosvela@gmail.com>
2025-06-09 09:24:47 -0600

    Making a CRUD operation using AXIOS

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { getPosts } from "../services/postService";
+import { getPosts, deletePost } from "../services/postService";
+import PostForm from "./PostForm";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [editingPost, setEditingPost] = useState(null);
 
   useEffect(() => {
     getPosts()
@@ -14,14 +16,36 @@ function Posts() {
       });
   }, []);
 
+  const handleDelete = (id) => {
+    deletePost(id)
+      .then(() => {
+        setPosts(posts.filter((post) => post.id !== id));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const startEditing = (post) => {
+    setEditingPost(post);
+  };
+
   return (
     <div>
       <h1>Posts of the day</h1>
+      <PostForm
+        posts={posts}
+        setPosts={setPosts}
+        editingPost={editingPost}
+        setEditingPost={setEditingPost}
+      />
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
+            <button onClick={() => startEditing(post)}>Edit</button>
+            <button onClick={() => handleDelete(post.id)}>Delete</button>
           </li>
         ))}
       </ul>
